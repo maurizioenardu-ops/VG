f05f0504cdce0033128be4bd06d8b8fff25d5380
maurizioenardu-ops <maurizio.enardu@gmail.com>
2026-03-18 10:29:04 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='vg-cache-v9_1';
-const VERSION = 'v8_9c';
+const CACHE_NAME='vg-cache-v9_4';
+const VERSION = 'v9_4';
 const CACHE = `vg-runtime-${VERSION}`;
 const STATIC_ASSETS = [
   './manifest.json',
