5a3c682ac7304f0d35938a849802300d9f026fda
maurizioenardu-ops <maurizio.enardu@gmail.com>
2026-05-14 18:53:26 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = 'gestionale-2026-05-14-finali-30-no-desc-v70-foto-dirette';
+const VERSION = 'gestionale-2026-05-14-finali-30-no-desc-v71-no-pin';
 const CACHE = `gestionale-runtime-${VERSION}`;
 const STATIC_ASSETS = [
   './',
