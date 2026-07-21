0490a4dafe9422f215f36c7229005c0b95007879
maurizioenardu-ops <maurizio.enardu@gmail.com>
2026-07-21 19:11:57 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = 'gestionale-vg-1.0.20-2026-07-18';
+const VERSION = 'gestionale-vg-1.0.21-2026-07-21';
 const CACHE = `gestionale-runtime-${VERSION}`;
 const REQUIRED_ASSETS = [
   './', './index.html', './manifest.json',
