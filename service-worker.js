c346d4bd10154cc603fa265478284640c21f720d
meapps-it <maurizio.enardu@gmail.com>
2026-09-09 18:53:47 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,9 @@
-const VERSION = 'gestionale-vg-1.0.115-seconda-app-2026-09-08';
+const VERSION = 'gestionale-vg-1.0.116-minimal';
 const CACHE = `gestionale-runtime-${VERSION}`;
 const REQUIRED_ASSETS = [
   './', './index.html', './manifest.json',
   './icon-v1-192.png', './icon-v1-512.png', './icon-v1-maskable-512.png',
   './apple-touch-icon-v1.png', './favicon-v1-32.png',
-  './screenshot-mobile-v1.jpg', './screenshot-wide-v1.jpg'
 ];
 
 self.addEventListener('install', event => {
